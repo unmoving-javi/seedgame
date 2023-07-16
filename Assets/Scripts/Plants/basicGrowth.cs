2e4f14c823e22a4c9b2cd8182960d6e2491ef7c7
unmoving-javi <62627114+unmoving-javi@users.noreply.github.com>
2023-07-16 15:24:40 -0700

    nested growth cycles fixed
    basicGrowth should now work as intended
    
    give or take some jank.

--- Assets/Scripts/Plants/basicGrowth.cs
+++ Assets/Scripts/Plants/basicGrowth.cs
@@ -18,6 +18,7 @@ public class basicGrowth : MonoBehaviour
     // for internal use
     Transform thisTrans = null;
     Vector3 initialScale = new Vector3(1,1,1);
+    Vector3 initialPos = new Vector3(0,0,0);
 
     //if true, the child object of this plant will become a physics object when grown
     public bool physWhenGrown = false;
@@ -31,21 +32,24 @@ public class basicGrowth : MonoBehaviour
     {
         thisTrans = this.gameObject.transform;
         initialScale= thisTrans.localScale;
+        initialPos = thisTrans.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (!(currentGrowSeconds >= timeToGrow)) {
-            float scale = 0.00001f;
+        if (!(currentGrowSeconds >= timeToGrow+delayToGrow)) {
+            float scale = 0.01f;
             currentGrowSeconds += Time.deltaTime;
             if (!(currentGrowSeconds < delayToGrow)){
-                scale = Mathf.Lerp(0.001f, scaleWhenGrown, (currentGrowSeconds+delayToGrow)/(timeToGrow+delayToGrow));
+                scale = Mathf.Lerp(0.001f, scaleWhenGrown, (currentGrowSeconds - delayToGrow)/(timeToGrow));
             }
 
 
             thisTrans.localScale = new Vector3(scale*initialScale.x, scale*initialScale.y, scale*initialScale.z);
+
+            //thisTrans.localPosition = new Vector3(scale*initialPos.x, scale*initialPos.y, scale*initialPos.z);
         } else if (physWhenGrown && thisTrans.childCount >= 1 && !grown){
             Rigidbody2D plantPhys = thisTrans.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
 
